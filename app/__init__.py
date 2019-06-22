6b208d3d6d629b19efa68f1d5a8414348c128385
esther <estherkc100@gmail.com>
2019-06-22 12:00:06 +0300

    added new file templates

--- app/__init__.py
+++ app/__init__.py
@@ -14,4 +14,13 @@ app.config.from_object(config_options[config_name])
 #initializing flask extensions
 bootstrap.init_app(app)
 
+# Registering the blueprint
+    from .main import main as main_blueprint
+    app.register_blueprint(main_blueprint)
+
+# Setting config
+    from .requests import configure_request
+    configure_request(app)
+
+    
 return app
---
+++ start.sh
@@ -0,0 +1,4 @@
+export NEWS_API_KEY ='8042f30000994df78898ee8fafe63983'
+export SECRET_KEY ='1318c9aa'
+
+python3.6 manage.py server
