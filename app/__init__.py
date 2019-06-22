24459c1d0448d87069b407f65547195b60c7e30a
esther <estherkc100@gmail.com>
2019-06-22 13:17:00 +0300

    created forms file

--- app/__init__.py
+++ app/__init__.py
@@ -1,26 +1,25 @@
-from flask import flask
-from flask_bootsrap import flask_bootsrap
+from flask import Flask
 from config import config_options
+from flask_bootsrap import Bootstrap
 
-bootsrap = Bootsrap()
+bootstrap = Bootstrap()
 
-def create_app(config_name)
 
-app = Flask(__name__)
+def create_app(config_name):
+    app = Flask(__name__)
 
-#creating app configuarations
-app.config.from_object(config_options[config_name])
+    # Creating the app configurations
+    app.config.from_object(config_options[config_name])
 
-#initializing flask extensions
-bootstrap.init_app(app)
+    # Initializing flask extensions
+    bootstrap.init_app(app)
 
-# Registering the blueprint
+    # Registering the blueprint
     from .main import main as main_blueprint
     app.register_blueprint(main_blueprint)
 
-# Setting config
+    # Setting config
     from .requests import configure_request
     configure_request(app)
 
-    
-return app
+    return app
--- start.sh
+++ start.sh
@@ -1,4 +1,4 @@
-export NEWS_API_KEY ='8042f30000994df78898ee8fafe63983'
-export SECRET_KEY ='1318c9aa'
+export NEWS_API_KEY = '8042f30000994df78898ee8fafe63983'
+export SECRET_KEY = ''
 
 python3.6 manage.py server
